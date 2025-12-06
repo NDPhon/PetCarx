f94410c06e617ff2b41f77f8b212cea742892f40
Phon <ndphon23@clc.fitus.edu.vn>
2025-12-06 20:30:30 +0700

    config backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config/db.js";
+dotenv.config();
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+const PORT = process.env.BACKEND_PORT || 8000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
